36e1cfda9d6e1c03838e74b390e04fa44cfadbdd
Luís Ferreira <lsferreira169@gmail.com>
2016-01-17 15:02:09 +0000

    Update JetBrains IDEA & Config File

--- config/cfg.php
+++ config/cfg.php
@@ -1,3 +1,9 @@
+<!-- 
+Yb    dP 88  dP""b8 88 88      dP"Yb  
+ Yb  dP  88 dP   `" 88 88     dP   Yb 
+  YbdP   88 Yb  "88 88 88  .o Yb   dP 
+   YP    88  YboodP 88 88ood8  YbodP  
+-->
 <?php
 //Databse Account Cerdentials
 $hostname = "";
