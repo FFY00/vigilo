d0bf73dac6811cbed790f36aeb211237d732274d
Luís Ferreira <lsferreira169@gmail.com>
2016-01-25 21:55:05 +0000

    Update
    + HTML5 Support
    + Update style file
    + Many more ...

--- gamedir/login/index.php
+++ gamedir/login/index.php
@@ -4,48 +4,81 @@ Yb    dP 88  dP""b8 88 88      dP"Yb
   YbdP   88 Yb  "88 88 88  .o Yb   dP 
    YP    88  YboodP 88 88ood8  YbodP  
 -->
-<!DOCTYPE HTML>
-	<html>
+<!DOCTYPE html>
+	<html lang="en">
 		<head>
+		<!--[if IE]>
+			<meta content="true" name="MSSmartTagsPreventParsing">
+    		<meta content="false" http-equiv="imagetoolbar">
+    	<![endif]-->
 
-	<!--[if lt IE 7]>
-		<style type="text/css">
-			#wrapper { height:100%; }
-			#footer {
-			position:fixed;
-			bottom:0; }
-		</style>
-	<![endif]-->
+		<!--[if lt IE 7]>
+			<style type="text/css">
+				#wrapper { height:100%; }
+				#footer {
+				position:fixed;
+				bottom:0; }
+			</style>
+		<![endif]-->
+
+		<!--[if lt IE 9]>
+			<script src="res/reveal-js/lib/js/html5shiv.js"></script>
+			<script src="http://html5shiv-printshiv.googlecode.com/svn/trunk/html5shiv-printshiv.js"></script>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script> 
+		<![endif]-->
 	
 			<title>Login Area</title>
-			<?php require_once("../../config/cfg.php"); ?>
-			<?php googleanalytics(); ?>
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+				<!-- Meta TAG's -->
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
+				<!-- Apple Phones Optimization -->
+			<meta name="apple-mobile-web-app-capable" content="yes">
+			<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+			<meta name="description" content="Web-Based Hacking simulation game">
+			<meta content="hacking, hack, vigilo, vigiloproject, game, simulator, web-based game" name="keywords">
+			<meta name="author" content="Vigilo">
+			<meta name="referrer" content="origin">
 			<meta charset="UTF-8">
+
 			<?php 
-				echo '<link rel="icon" href="'.$root_remotepath.'/res/favicon.ico" type="image/x-icon"/>';
-				echo '<link rel="shortcut icon" href="'.$root_remotepath.'/res/favicon.ico">';
-				echo '<link rel="stylesheet" type="text/css" href="'.$root_remotepath.'/res/main.css">';
-				echo '<link href="'.$root_remotepath.'/res/bootstrap/css/bootstrap.css" rel="stylesheet">';
-				echo '<link href="'.$root_remotepath.'/res/bootstrap/css/bootstrap.min.css" rel="stylesheet">';
-				echo '<script src="'.$root_remotepath.'/res/bootstrap/js/bootstrap.min.js"></script>';
-				echo '<script src="'.$root_remotepath.'/res/smoothscroll/smoothscroll.js"></script>';
-				echo '<script src="'.$root_remotepath.'/res/jquery/jquery.js"></script>';
-				echo '<script src="'.$root_remotepath.'/res/jquery/jquery-2.1.3.min.js"></script>';
-				echo '<link rel="stylesheet" type="text/css" href="'.$root_remotepath.'/res/jquery-eu-cookie-law-popup/jquery-eu-cookie-law-popup.css"/>';
-				echo '<script src="'.$root_remotepath.'/res/jquery-eu-cookie-law-popup/jquery-eu-cookie-law-popup.js"></script>';
-				echo '<script src="'.$root_remotepath.'/res/vigilo-js/index.js"></script>';
-			 ?>
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-			<script src='https://www.google.com/recaptcha/api.js'></script>
+				require_once("../../config/cfg.php"); 
+				require_once("../../res/vigilolibrary.php");
+
+				echo '
+					<link rel="icon" href="'.$root_remotepath.'/res/favicon.ico" type="image/x-icon"/>
+					<link rel="shortcut icon" href="'.$root_remotepath.'/res/favicon.ico" type="image/x-icon"/>
+					<link rel="shortcut icon" href="'.$root_remotepath.'/res/favicon.ico">
+
+					<link rel="stylesheet" type="text/css" href="'.$root_remotepath.'/res/style.css">
+					<link rel="stylesheet" type="text/css" href="'.$root_remotepath.'/res/background.css">
+
+					<script src="'.$root_remotepath.'/res/jquery/jquery-1.12.0.min.js"></script>
+					<!-- <script src="'.$root_remotepath.'/res/jquery/jquery-2.2.0.min.js"></script> -->
+
+					<script src="https://www.google.com/recaptcha/api.js"></script>
+
+					<link rel="stylesheet" type="text/css" href="'.$root_remotepath.'/res/jquery-eu-cookie-law-popup/jquery-eu-cookie-law-popup.css"/>
+					<script src="'.$root_remotepath.'/res/jquery-eu-cookie-law-popup/jquery-eu-cookie-law-popup.js"></script>
+
+					<link href="'.$root_remotepath.'/res/bootstrap/css/bootstrap.css" rel="stylesheet">
+					<link href="'.$root_remotepath.'/res/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+					<link type="text/css" href="'.$root_remotepath.'/res/normalize/normalize.css" rel="stylesheet">
+					<script src="'.$root_remotepath.'/res/bootstrap/js/bootstrap.min.js"></script>
+
+					<script src="'.$root_remotepath.'/res/vigilo-js/index.js"></script>
+				';
+
+				googleanalytics($google_ua_id);
+			?>
+
 			<noscript>
-				Your browser does not support JavaScript or it is disabled!
+				Sorry, but Vigilo does not work if you do not have Javascript enabled. Your browser does not support JavaScript or it is disabled!
 			</noscript>
 		</head>
 		<body class="eupopup eupopup-top">
 			<div id="wrapper">
-				<form class="form-horizontal" method="post">
-					<fieldset>
+				<form role="form" class="form-horizontal" method="post">
 						<div class="container">
 							<div class="row">
 								<div id="header">
@@ -69,7 +102,7 @@ Yb    dP 88  dP""b8 88 88      dP"Yb
   										</div>
 									</div>
 
-									<center>
+									<div id="center">
 										<div class="g-recaptcha" data-sitekey=<?php echo '"' . $captchapublickey . '"'; ?>></div></br>
 										<label for="checkbox"><input name="checkbox" id="checkbox-remember" value="1" type="checkbox" checked> Remember account?</label>
 											<!-- Button (Double) -->
@@ -77,19 +110,18 @@ Yb    dP 88  dP""b8 88 88      dP"Yb
 										<button id="btlogin" class="btn btn-info" formaction="/login/validation/">Login</button>
 										<button id="btregister" class="btn btn-success" formaction="/register/">Register</button>
 
-									</center>
+									</div>
 								</div>
 							</div>
 						</div>
-					</fieldset>
 				</form>
 
 				<div id="footer">
-					<center>
+					<div id="center">
 						<h6>
 							<b>Github: </b><a href="https://github.com/vigiloproject">https://github.com/vigiloproject</a>
 						</h6>
-					</center>
+					</div>
 				</div>
 			</div>
 		</body>
