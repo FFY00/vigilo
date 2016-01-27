70c2eae09039560c4e3b8b52ea1d9534804c16a8
Luís Ferreira <lsferreira169@gmail.com>
2016-01-27 22:05:17 +0000

    Update
    + Update validation script

--- gamedir/login/validation/index.php
+++ gamedir/login/validation/index.php
@@ -5,12 +5,12 @@ Yb    dP 88  dP""b8 88 88      dP"Yb
    YP    88  YboodP 88 88ood8  YbodP  
 -->
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 <head>
 	<title>...</title>
 		<!-- Meta TAGs -->
 			<?php require_once("../../../config/cfg.php"); ?>
-			<?php googleanalytics(); ?>
+			<?php googleanalytics($google_ua_id); ?>
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 			<meta charset="UTF-8">
 			<?php 
@@ -42,7 +42,6 @@ Yb    dP 88  dP""b8 88 88      dP"Yb
 	$usr = $_POST["usr"];
 	$passwd = $_POST["passwd"];
 	$checkbox = $_POST["checkbox"];
-	echo $checkbox;
 	$captcharesponse = $_POST["g-recaptcha-response"];
 
 //extract data from the post
@@ -157,26 +156,6 @@ USEREMPTY;
 	exit;
 	}
 
-$query = "SELECT * FROM users WHERE username='" . $usr . "'";
-if(!($usr == NULL)){
-if(!($db->query($query)->fetchColumn()) > 0){
-    echo <<<USER
-<div id="header">
-	<center><h1>Wrong Username!</h1><br>
-</div>
-<div id="content">
-<center>
-<p>If you are not redirected automatically, follow the <a href="/register">link</a>.</p>
-</center>
-<script type="text/javascript"> setTimeout("window.location.href = ' . "'/register'" . '", 5000); </script>
-<meta http-equiv="refresh" content="5;url=/register">
-</div>
-</div>
-USER;
-    exit;
-}
-}
-
 	if ($passwd ==NULL)
 	{
 		echo <<<PASSWORDEMPTY
@@ -208,7 +187,25 @@ PASSWORDEMPTY;
 		//Generate password in sha512
 	$generated_passwd = hash('sha512', $passwd);
 
-	
+
+	//read mysql database
+$query = "SELECT * FROM users WHERE email='" . $email . "' and password='".$passwd."'";
+if(($db->query($query)->fetchColumn()) > 0){
+    echo <<<LOGIN
+<div id="header">
+	<center><h1>Login successfully!</h1><br>
+</div>
+<div id="content">
+<center>
+<p>If you are not redirected automatically, follow the <a href="/register">link</a>.</p>
+</center>
+<script type="text/javascript"> setTimeout("window.location.href = ' . "'/register'" . '", 5000); </script>
+<meta http-equiv="refresh" content="5;url=/register">
+</div>
+</div>
+LOGIN;
+    exit;
+}
 
 ?>
 <div id="footer">
