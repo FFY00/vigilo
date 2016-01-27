da86b6ff19f06d3bb20a69267e44469f518d5442
Luís Ferreira <lsferreira169@gmail.com>
2016-01-27 00:29:40 +0000

    Update
    + Update vigilo-js library
    + Update CSS file
    + Add temporary files (bootsnipp and msg file)

--- res/vigilo-js/index.js
+++ res/vigilo-js/index.js
@@ -111,3 +111,29 @@ return function( pDiv, pSrc, pFunc ){  // public
   }
   }
 })();
+
+$(document).ready(function(){
+
+$(function(){
+ 
+    $(document).on( 'scroll', function(){
+ 
+      if ($(window).scrollTop() > 100) {
+      $('.scroll-top-wrapper').addClass('show');
+    } else {
+      $('.scroll-top-wrapper').removeClass('show');
+    }
+  });
+ 
+  $('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+  verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+  element = $('body');
+  offset = element.offset();
+  offsetTop = offset.top;
+  $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+
+});
