bab6d4a8ad8a4ff125c0bdeedce0659bd9626671
Luís Ferreira <lsferreira169@gmail.com>
2016-02-12 00:22:38 +0000

    Update
    * Update Session name (edit all session support files)

--- gamedir/logout/index.php
+++ gamedir/logout/index.php
@@ -4,7 +4,9 @@ Yb    dP 88  dP""b8 88 88      dP"Yb
   YbdP   88 Yb  "88 88 88  .o Yb   dP 
    YP    88  YboodP 88 88ood8  YbodP  
 -->
-<?php session_start(); ?>
+<?php 
+session_name("VigiloID");
+session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
